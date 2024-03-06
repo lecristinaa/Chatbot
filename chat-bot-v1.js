cbfba22532b1e613cba5cc6c1c7136a76762226e
lê <112022669+lecristinaa@users.noreply.github.com>
2024-03-06 17:23:06 -0300

    Update chat-bot-v1.js

--- chat-bot-v1.js
+++ chat-bot-v1.js
@@ -1,41 +1,83 @@
-import { palavrasFutebol, palavrasMundialPalmeiras, palavrasRegras, palavrasTimeGosta, papoFurado, mostProbablyQuestion } from './can-i-answer.js'
-// O que ele vai poder responder?
-// - O que é futebol?
-// - Quais as regras do futebol?
-// - Que time ele mais gosta?
-// - Palmeiras tem mundial?
-// - PAPO FURADO?
+// O que é futebol?
+export const palavrasFutebol = {
+    probablyWords: [
+    "que", "futebol",
+    "q", "é", "e",
+    "me", "explica",
+    "sabe", "dizer", "o",
+    ],
+    matched: 0,
+}
 
-// Separe a frase em uma array de palavras
-let pergunta = "Basquete pode ser considerado um esporte ?"
-pergunta = pergunta.toLowerCase()
-pergunta = pergunta.split(' ')
-// Tirar da pergunta o "?"
-pergunta = pergunta.slice(0, 6)
+// Quais as regras do futebol?
+export const palavrasRegras = {
+    probablyWords: [
+    "quais", "as", "regras", "do", "futebol",
+    "sao", "como", "funciona",
+    ],
+    matched: 0,
+}
+
+// Que time ele mais gosta?
+export const palavrasTimeGosta = {
+    probablyWords: [
+    "que", "mais", "gosta",
+    "qual", "você", "torce",
+    "preferido", "clube", "coração",
+    "do", "time",
+    ],
+    matched: 0,
+}
+
+// Palmeiras tem mundial?
+export const palavrasMundialPalmeiras = {
+    probablyWords: [
+    "palmeiras", "tem", "mundial",
+    "copa", "do", "mundo",
+    "campeão", "rio", "copa rio"
+    ],
+    matched: 0,
+}
+
+// Palavras que não tem haver com futebol
+export const papoFurado = {
+    probablyWords: [
+        "filme", "basquete", "unha",
+        "teclado", "vôlei", "natação",
+        "atletismo", "esgrima",
+    ],
+    matched: 0,
+}
 
 
-const bancos = [
-    palavrasFutebol,
-    palavrasMundialPalmeiras,
-    palavrasRegras,
-    palavrasTimeGosta,
-    papoFurado,
-]
-
-
-
-// Ande pela array de palavras
-  // Veja se a palavra existe no banco X, 
-  // senão, veja no Y, 
-  // senão, veja no Z...TEM NO Z? tensione (aumente o matched em 1)
-
-// Cada volta representa um elemento da array
-for (let i = 0; i < pergunta.length; i++) { // Palavra i
-    for (let j = 0; j < bancos.length; j++) { // Procura a palavra i, no banco j
-        if (bancos[j].probablyWords.indexOf(pergunta[i]) !== -1) {
-            bancos[j].matched++
-        }
+function showAnswer(index) {
+    switch (index) {
+        case 0:
+            return "O futebol é o esporte mais conhecido do mundo e mais difundido em todos os países, sendo o Brazil e a Europa os maiores!"
+        case 1:
+            return "Algumas regras do futebol seria: não pode agressão, não pode gol contra"
+        case 2:
+            return "Eu gosto do tigre"
+        case 3:
+            return "O palmeiras não tem mundial, mas tem copa RIO, mas eles falam que tem né, fazer oq"
+        default:
+            return "Só sei falar de futebol meu amigo, se não for isso, tchau"
     }
 }
 
-console.log(mostProbablyQuestion())
+// Função para saber qual, provavelmente, foi a pergunta feita
+export function mostProbablyQuestion() {
+    let matches = [
+        palavrasFutebol.matched, 
+        palavrasRegras.matched, 
+        palavrasTimeGosta.matched,
+        palavrasMundialPalmeiras.matched, 
+        papoFurado.matched,
+    ]
+
+    console.log(matches)
+    console.log(Math.max(...matches))
+    console.log(matches.indexOf(Math.max(...matches)))
+    // Vê quantas palavras de cada pergunta foi usada
+    return showAnswer(matches.indexOf(Math.max(...matches)))
+}
